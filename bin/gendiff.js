ae04033f28cf9f4c6083e9ea2be135b2e1cea11d
Anastasiia Piatiletova <Nasten4ik1996@mail.ru>
2021-12-28 20:35:13 +0700

    Add gendiff.js and commander.js

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import { Command } from 'commander/esm.mjs';
+
+const program = new Command();
+program
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format');
+
+
+program.parse(process.argv);
