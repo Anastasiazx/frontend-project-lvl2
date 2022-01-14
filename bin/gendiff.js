f8b976deaaa6f14c34dd3dcf4b6d56321a6a42e6
Anastasiia Piatiletova <Nasten4ik1996@mail.ru>
2022-01-14 18:55:18 +0700

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
-import { Command } from 'commander/esm.mjs';
+import { Command } from 'commander';
 import genDiff from '../src/diff.js';
 
 const program = new Command();
 program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .argument('<file1>')
-  .argument('<file2>')
-  .option('-f, --format [type]', 'output format')
+  .arguments('<file1> <file2>')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2);
+    const diff = genDiff(filepath1, filepath2, program.format);
     console.log(diff);
   });
 
