f16f43bbcb7fa50441a68ce571ee70f03db0836d
Anastasiia Piatiletova <Nasten4ik1996@mail.ru>
2022-01-14 15:01:15 +0700

    Update diff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,8 +6,8 @@ const program = new Command();
 program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
+  .argument('<file1>')
+  .argument('<file2>')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
     const diff = genDiff(filepath1, filepath2);
--- src/diff.js
+++ src/diff.js
@@ -1,29 +1,24 @@
 import { readFileSync } from 'fs';
 import _ from 'lodash';
 
-const data1 = readFileSync('./files/file1.json');
-const data2 = readFileSync('./files/file2.json');
-
 const genDiff = (object1, object2) => {
-  const result = [];
-  const file1 = JSON.parse(data1);
-  const file2 = JSON.parse(data2);
+  const file1 = JSON.parse(readFileSync(`./files/${object1}`));
+  const file2 = JSON.parse(readFileSync(`./files/${object2}`));
   const keysObject1 = Object.keys(file1);
   const keysObject2 = Object.keys(file2);
   const keysObjects = _.union(keysObject1, keysObject2).sort();
 
-  for (const key of keysObjects) {
+  const keys = keysObjects.map((key) => {
     if (!_.has(file1, key)) {
-      result.push(`  + ${key}: ${file2[key]}`);
-    } else if (!_.has(file2, key)) {
-      result.push(`  - ${key}: ${file1[key]}`);
-    } else if (file1[key] !== file2[key]) {
-      result.push(`  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`);
-    } else {
-      result.push(`    ${key}: ${file1[key]}`);
+      return `  + ${key}: ${file2[key]}`;
+    } if (!_.has(file2, key)) {
+      return `  - ${key}: ${file1[key]}`;
+    } if (file1[key] !== file2[key]) {
+      return `  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`;
     }
-  }
-  return `{\n${result.join('\n')} \n}`;
+    return `    ${key}: ${file1[key]}`;
+  });
+  return `{\n${keys.join('\n')} \n}`;
 };
 
 export default genDiff;
