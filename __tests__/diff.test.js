fa011824769c286ebf7ce1272ccdd5012a2b4b59
Anastasiia Piatiletova <Nasten4ik1996@mail.ru>
2022-01-18 17:05:33 +0700

    Update diff.test.js

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,15 +1,13 @@
-import genDiff from '../src/diff.js';
 import fs from 'fs';
-import { dirname } from 'path';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import path from 'path';
+import genDiff from '../src/diff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('test 1', () => {
+test('genDiff JSON', () => {
   expect(genDiff('file1.json', 'file2.json')).toEqual(readFile('expected_json.txt'));
 });
-
