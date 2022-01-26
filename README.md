2e95e1313672bdfe25f7122ed8e006c823e78741
Anastasiia Piatiletova <Nasten4ik1996@mail.ru>
2022-01-26 20:25:51 +0700

    Update diff.test.js

--- README.md
+++ README.md
@@ -5,10 +5,17 @@
 
 <a href="https://codeclimate.com/github/Anastasiazx/frontend-project-lvl2/test_coverage"><img src="https://api.codeclimate.com/v1/badges/589a96ebd895bf5aa2d1/test_coverage" /></a>
 
+GenDiff JSON files
 <a href="https://asciinema.org/a/ZGohxEpHXDb5UT40pCCs7URIf" target="_blank"><img src="https://asciinema.org/a/ZGohxEpHXDb5UT40pCCs7URIf.svg" /></a>
 
+GenDiff YAML files
 <a href="https://asciinema.org/a/JnGzB9Ans3xnwtmLDn4BtItW7" target="_blank"><img src="https://asciinema.org/a/JnGzB9Ans3xnwtmLDn4BtItW7.svg" /></a>
 
+Format 'stylish'
 <a href="https://asciinema.org/a/KPQ1yAZCJb8OFWes1LCEZUH1d" target="_blank"><img src="https://asciinema.org/a/KPQ1yAZCJb8OFWes1LCEZUH1d.svg" /></a>
 
+Format 'plain'
 <a href="https://asciinema.org/a/zGGkoPawXH38Oig7M4B0kqt7K" target="_blank"><img src="https://asciinema.org/a/zGGkoPawXH38Oig7M4B0kqt7K.svg" /></a>
+
+Format 'json'
+<a href="https://asciinema.org/a/QXVTaVbzM6GYaUOIjxrEv2VVt" target="_blank"><img src="https://asciinema.org/a/QXVTaVbzM6GYaUOIjxrEv2VVt.svg" /></a>
--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -9,6 +9,10 @@ test('genDiff YML, format = stylish', () => {
   expect(genDiff('filepath1.yaml', 'filepath2.yaml')).toEqual(readFile('expected_filepath_stylish.txt'));
 });
 
+test('genDiff JSON & YML, format = stylish', () => {
+  expect(genDiff('filepath1.json', 'filepath2.yaml')).toEqual(readFile('expected_filepath_stylish.txt'));
+});
+
 test('genDiff JSON, format = plain', () => {
   expect(genDiff('filepath1.json', 'filepath2.json', 'plain')).toEqual(readFile('expected_filepath_plain.txt'));
 });
@@ -17,6 +21,18 @@ test('genDiff YML, format = plain', () => {
   expect(genDiff('filepath1.yaml', 'filepath2.yaml', 'plain')).toEqual(readFile('expected_filepath_plain.txt'));
 });
 
+test('genDiff JSON & YML, format = plain', () => {
+  expect(genDiff('filepath1.json', 'filepath2.yaml', 'plain')).toEqual(readFile('expected_filepath_plain.txt'));
+});
+
 test('genDiff JSON, format = json', () => {
   expect(genDiff('filepath1.json', 'filepath2.json', 'json')).toEqual(readFile('expected_filepath_json.txt'));
+});
+
+test('genDiff YAML, format = json', () => {
+  expect(genDiff('filepath1.yaml', 'filepath2.yaml', 'json')).toEqual(readFile('expected_filepath_json.txt'));
+});
+
+test('genDiff JSON & YML, format = json', () => {
+  expect(genDiff('filepath1.json', 'filepath2.yaml', 'json')).toEqual(readFile('expected_filepath_json.txt'));
 });
