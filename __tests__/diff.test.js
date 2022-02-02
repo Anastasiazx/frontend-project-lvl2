728cdc54b030d6acd03c9144516e48930c574cf7
Anastasiia Piatiletova <Nasten4ik1996@mail.ru>
2022-02-02 11:34:17 +0700

    Update diff.test.js

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -9,37 +9,37 @@ const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filena
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('genDiff JSON, format = stylish', () => {
-  expect(genDiff('filepath1.json', 'filepath2.json')).toEqual(readFile('expected_filepath_stylish.txt'));
+  expect(genDiff(getFixturePath('filepath1.json'), getFixturePath('filepath2.json'))).toEqual(readFile('expected_filepath_stylish.txt'));
 });
 
 test('genDiff YML, format = stylish', () => {
-  expect(genDiff('filepath1.yaml', 'filepath2.yaml')).toEqual(readFile('expected_filepath_stylish.txt'));
+  expect(genDiff(getFixturePath('filepath1.yaml'), getFixturePath('filepath2.yaml'))).toEqual(readFile('expected_filepath_stylish.txt'));
 });
 
 test('genDiff JSON & YML, format = stylish', () => {
-  expect(genDiff('filepath1.json', 'filepath2.yaml')).toEqual(readFile('expected_filepath_stylish.txt'));
+  expect(genDiff(getFixturePath('filepath1.json'), getFixturePath('filepath2.yaml'))).toEqual(readFile('expected_filepath_stylish.txt'));
 });
 
 test('genDiff JSON, format = plain', () => {
-  expect(genDiff('filepath1.json', 'filepath2.json', 'plain')).toEqual(readFile('expected_filepath_plain.txt'));
+  expect(genDiff(getFixturePath('filepath1.json'), getFixturePath('filepath2.json'), 'plain')).toEqual(readFile('expected_filepath_plain.txt'));
 });
 
 test('genDiff YML, format = plain', () => {
-  expect(genDiff('filepath1.yaml', 'filepath2.yaml', 'plain')).toEqual(readFile('expected_filepath_plain.txt'));
+  expect(genDiff(getFixturePath('filepath1.yaml'), getFixturePath('filepath2.yaml'), 'plain')).toEqual(readFile('expected_filepath_plain.txt'));
 });
 
 test('genDiff JSON & YML, format = plain', () => {
-  expect(genDiff('filepath1.json', 'filepath2.yaml', 'plain')).toEqual(readFile('expected_filepath_plain.txt'));
+  expect(genDiff(getFixturePath('filepath1.json'), getFixturePath('filepath2.yaml'), 'plain')).toEqual(readFile('expected_filepath_plain.txt'));
 });
 
 test('genDiff JSON, format = json', () => {
-  expect(genDiff('filepath1.json', 'filepath2.json', 'json')).toEqual(readFile('expected_filepath_json.txt'));
+  expect(genDiff(getFixturePath('filepath1.json'), getFixturePath('filepath2.json'), 'json')).toEqual(readFile('expected_filepath_json.txt'));
 });
 
 test('genDiff YAML, format = json', () => {
-  expect(genDiff('filepath1.yaml', 'filepath2.yaml', 'json')).toEqual(readFile('expected_filepath_json.txt'));
+  expect(genDiff(getFixturePath('filepath1.yaml'), getFixturePath('filepath2.yaml'), 'json')).toEqual(readFile('expected_filepath_json.txt'));
 });
 
 test('genDiff JSON & YML, format = json', () => {
-  expect(genDiff('filepath1.json', 'filepath2.yaml', 'json')).toEqual(readFile('expected_filepath_json.txt'));
+  expect(genDiff(getFixturePath('filepath1.json'), getFixturePath('filepath2.yaml'), 'json')).toEqual(readFile('expected_filepath_json.txt'));
 });
